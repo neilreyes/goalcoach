b8985d9027122852b230e1e590f68962785a1917
Rayneil Reyes <rayneilreyes@gmail.com>
2018-11-12 20:42:25 +0800

    Added react-redux and redux to npm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.handleSignOut = this.handleSignOut.bind(this);
+
+    }
+
+    handleSignOut(){
+        firebaseApp.auth().signOut();
+    }
+
     render(){
         return (
             <div className="container">
                 <h1>App</h1>
+                <button
+                    className="btn btn-danger"
+                    onClick={this.handleSignOut}
+                >
+                    Sign Out
+                </button>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import { firebaseApp } from './firebase';
 
@@ -10,8 +12,10 @@ import SignUp from './components/SignUp';
 firebaseApp.auth().onAuthStateChanged(user=>{
     if(user){
         console.log('User has sign in or up', user);
+        browserHistory.push('/app');
     } else {
-        console.log('False: ', user);
+        console.log('User has signed out or still need to sign in.: ');
+        browserHistory.replace('/signin');
     }
 });
 
