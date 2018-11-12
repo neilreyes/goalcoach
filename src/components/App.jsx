55bb99da602966fa3d6072ce81f0e38632ef07b8
Neil Reyes <rayneilreyes@gmail.com>
2018-11-12 14:32:11 +0800

    Implements react-router 3.0.1 to the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
 
 class App extends Component{
     render(){
         return (
             <div>
                 <h1>App</h1>
-                <SignIn />
-                <SignUp />
             </div>
         )
     }
